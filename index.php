bf4a7d1e9b3c273fff89ae404716c6d84eef8dff
Your Name <you@example.com>
2018-06-07 15:37:34 +0800

    12345

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+﻿<?php 
 header("content-type:text/html;charset=utf8");
 mysql_connect('127.0.0.1','root','root');
 mysql_query("set names utf8");
@@ -9,6 +9,6 @@ $sql = "insert into userarea(`username`,`userarea`) value('$username','$area')";
 if(mysql_query($sql)){
 	echo "添加成功";
 }else{
-	mysql_error();die;
+	mysql_error();
 }
 ?>
